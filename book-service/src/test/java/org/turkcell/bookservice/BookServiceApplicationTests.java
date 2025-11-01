2a0a8cd0ec5817fb0c47ea5859d2fe7461b1f094
Cihan-KARA-hub <cihankara632@gmail.com>
2025-11-01 16:54:17 +0300

    start: init microservice project

---
+++ book-service/src/test/java/org/turkcell/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turkcell.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ penalty-service/src/test/java/org/turkcell/penaltyservice/PenaltyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turkcell.penaltyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenaltyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
